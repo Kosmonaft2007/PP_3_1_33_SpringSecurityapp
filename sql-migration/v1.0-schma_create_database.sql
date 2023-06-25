7ea189490501e8fb404704e1ab3978bf57a9f805
Vadim <kosmonaft2007@ya.ru>
2023-06-25 19:34:16 +0300

    branch DEMO non maste

--- sql-migration/v1.0-schma_create_database.sql
+++ sql-migration/v1.0-schma_create_database.sql
@@ -23,8 +23,8 @@ CREATE TABLE users_roles
     user_id INT NOT NULL,
     role_id INT NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
 
     UNIQUE (user_id, role_id)
 )
